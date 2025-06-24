3c04a0303a2057514fb8a063ef628a835c9da58d
Swaynel <152862828+Swaynel@users.noreply.github.com>
2025-06-25 02:57:29 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,23 @@
 // js/firebase.js
-// Config
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+
+// Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-// App setup
-edit
-
-System: // App setup
-import { initializeApp } from "https://www.gstatic.com/firebase/9.24.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebase/9.24.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebase/9.24.1/firebase-auth.js";
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
